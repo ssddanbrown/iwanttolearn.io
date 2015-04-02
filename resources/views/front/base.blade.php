b5e08814fd03c5d78303a2c82ad22627600c2c52
Dan Brown <ssddanbrown@googlemail.com>
2015-04-02 20:36:27 +0100

    Added google analytics and environment variable

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -15,6 +15,19 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
+    @if(env('GA_TRACKING_ID', false))
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', '{{ env('GA_TRACKING_ID') }}', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+    @endif
+
 </head>
 <body ng-app="learn">
 <nav class="navbar navbar-default">
