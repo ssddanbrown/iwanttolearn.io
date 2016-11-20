5bf8abbe3253ce89438a514ff29ffd146296abd0
Dan Brown <ssddanbrown@googlemail.com>
2016-11-20 17:01:54 +0000

    Updated e2e tests to work with app

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,18 +2,39 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
+  'page tests': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL
 
     browser
+      // Check home page
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.home')
+      .assert.containsText('h1', 'I want to learn...')
+      // .assert.elementCount('img', 1)
+
+      // Check tag page
+      .url(devServer + '/t/php')
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.tag-page')
+      .assert.elementPresent('.masonry-item')
+      .assert.containsText('.masonry', 'Laracasts')
+
+
+      // Check about page
+      .url(devServer + '/about')
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('.panel', 'To collect the best resources for a vast range of subjects into a single point of reference.')
+
+      // Check format page
+      .url(devServer + '/f/video')
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('h1', 'Resources In Video Format')
+      .assert.containsText('.format','Laracasts')
+
       .end()
   }
 }
